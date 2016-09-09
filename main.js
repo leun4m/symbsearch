cfdb1c652ecc2cfa8f1f3e9a168a3ac40a55b8f7
Leun4m <manuel@mi55ion.de>
2016-09-09 16:41:38 +0200

    made Window frameless

--- main.js
+++ main.js
@@ -6,13 +6,10 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 400, height: 400, frame: false, resizable: false})
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`)
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
