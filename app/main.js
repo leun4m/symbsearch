e4cef171dd25090aec00629a2f48774c875c8ceb
Leun4m <manuel@mi55ion.de>
2016-10-12 13:16:23 +0200

    New structure

--- app/main.js
+++ app/main.js
@@ -8,12 +8,12 @@ let tray = null
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: config.width, height: config.height, frame: false, resizable: false})
+  win = new BrowserWindow({width: config.width, height: config.height, icon: __dirname + '/style/icon32.png', frame: false, resizable: false})
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`)
 
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
   // Emitted when the window is closed.
   win.on('closed', () => {
     // Dereference the window object, usually you would store windows
